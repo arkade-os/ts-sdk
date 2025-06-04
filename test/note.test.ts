3c08cfcd8b1301ca7aa39998b5c2ff57748c7659
João Bordalo <bordalix@users.noreply.github.com>
2025-06-04 10:36:32 +0100

    refactor tests to test hrp

--- test/note.test.ts
+++ test/note.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { ArkAddress, ArkNote } from "../src";
+import { ArkNote } from "../src";
 import fixtures from "./fixtures/encoding.json";
 import { hex } from "@scure/base";
 
@@ -7,20 +7,22 @@ describe("ArkNote", () => {
     describe("valid notes", () => {
         fixtures.note.valid.forEach((fixture) => {
             it(`should correctly decode and encode note ${fixture.str}`, () => {
-                // Check HRP is valid
-                expect(fixture.str.slice(0, ArkNote.HRP.length)).toEqual(
-                    ArkNote.HRP
-                );
-
                 // Test decoding
-                const note = ArkNote.fromString(fixture.str);
+                const note = ArkNote.fromString(fixture.str, fixture.hrp);
                 const preimage = hex.encode(note.preimage);
                 expect(preimage).toBe(fixture.expectedPreimage);
                 expect(note.value).toBe(fixture.expectedValue);
 
                 // Test encoding
-                const newNote = new ArkNote(note.preimage, note.value);
+                const newNote = new ArkNote(
+                    hex.decode(fixture.expectedPreimage),
+                    fixture.expectedValue,
+                    fixture.hrp
+                );
                 expect(newNote.toString()).toBe(fixture.str);
+
+                // Check HRP is valid
+                expect(fixture.str.slice(0, note.HRP.length)).toEqual(note.HRP);
             });
         });
     });
