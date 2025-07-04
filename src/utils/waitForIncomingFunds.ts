a65fc2a0c04d32c57f153ac4fe723b55a4d526e2
João Bordalo <bordalix@users.noreply.github.com>
2025-07-04 15:38:19 +0100

    update src/utils/waitForIncomingFunds.ts

--- src/utils/waitForIncomingFunds.ts
+++ src/utils/waitForIncomingFunds.ts
@@ -1,6 +1,8 @@
 import { Coin, VirtualCoin, Wallet } from "..";
 
-export async function waitForIncomingFunds(wallet: Wallet): Promise<Coin[]> {
+export async function waitForIncomingFunds(
+    wallet: Wallet
+): Promise<Coin[] | VirtualCoin[]> {
     return new Promise((resolve) => {
         wallet.notifyIncomingFunds(
             (coins: Coin[] | VirtualCoin[], stopFunc) => {
