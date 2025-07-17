fa45809a58b2c98ca0831f81849081efcee68f15
João Bordalo <bordalix@users.noreply.github.com>
2025-07-17 15:58:07 +0100

    test vhtlc addresses

--- test/address.test.ts
+++ test/address.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { ArkAddress } from "../src";
+import { ArkAddress, RelativeTimelock, VHTLC } from "../src";
 import fixtures from "./fixtures/encoding.json";
 import { hex } from "@scure/base";
 
@@ -38,6 +38,77 @@ describe("ArkAddress", () => {
             it(`should fail to decode invalid address ${fixture.addr}`, () => {
                 expect(() => ArkAddress.decode(fixture.addr)).toThrow();
             });
+        });
+    });
+
+    describe("VHTLC Address", () => {
+        const preimageHash = "4d487dd3753a89bc9fe98401d1196523058251fc";
+        const receiver =
+            "021e1bb85455fe3f5aed60d101aa4dbdb9e7714f6226769a97a17a5331dadcd53b";
+        const receiverXOnly = receiver.slice(2);
+        const sender =
+            "030192e796452d6df9697c280542e1560557bcf79a347d925895043136225c7cb4";
+        const senderXOnly = sender.slice(2);
+        const server =
+            "03aad52d58162e9eefeafc7ad8a1cdca8060b5f01df1e7583362d052e266208f88";
+        const serverXOnly = server.slice(2);
+        const refundLocktime = 265n;
+        const unilateralRefundDelay: RelativeTimelock = {
+            type: "blocks",
+            value: 144n,
+        };
+        const unilateralRefundWithoutReceiverDelay: RelativeTimelock = {
+            type: "blocks",
+            value: 144n,
+        };
+        it("valid address with no values < 17", () => {
+            const unilateralClaimDelay: RelativeTimelock = {
+                type: "blocks",
+                value: 17n,
+            };
+            const vhtlcScript = new VHTLC.Script({
+                preimageHash: hex.decode(preimageHash),
+                sender: hex.decode(senderXOnly),
+                receiver: hex.decode(receiverXOnly),
+                server: hex.decode(serverXOnly),
+                refundLocktime,
+                unilateralClaimDelay,
+                unilateralRefundDelay,
+                unilateralRefundWithoutReceiverDelay,
+            });
+
+            const vhtlcAddress = vhtlcScript
+                .address("tark", hex.decode(serverXOnly))
+                .encode();
+
+            expect(vhtlcAddress).toBe(
+                "tark1qz4d2t2czchfaml2l3ad3gwde2qxpd0srhc7wkpnvtg99cnxyz8c3pnvvhnhumhwhqthmlxmdryakwx99s6508y8dunj9sty2p5mr7unh5re63"
+            );
+        });
+
+        it("valid address with some values < 17", () => {
+            const unilateralClaimDelay: RelativeTimelock = {
+                type: "blocks",
+                value: 16n,
+            };
+            const vhtlcScript = new VHTLC.Script({
+                preimageHash: hex.decode(preimageHash),
+                sender: hex.decode(senderXOnly),
+                receiver: hex.decode(receiverXOnly),
+                server: hex.decode(serverXOnly),
+                refundLocktime,
+                unilateralClaimDelay,
+                unilateralRefundDelay,
+                unilateralRefundWithoutReceiverDelay,
+            });
+
+            const vhtlcAddress = vhtlcScript
+                .address("tark", hex.decode(serverXOnly))
+                .encode();
+
+            expect(vhtlcAddress).toBe(
+                "tark1qz4d2t2czchfaml2l3ad3gwde2qxpd0srhc7wkpnvtg99cnxyz8c3vyn9exe9gjwcjp5ez0wfhhawvvg0xfenzztjmgp3ddrvkwhw04eztqjn6"
+            );
         });
     });
 });
