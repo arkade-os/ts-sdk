661e9573151c5ad4f97343de0b8d37f1529d566e
João Bordalo <bordalix@users.noreply.github.com>
2025-09-18 11:36:07 +0100

    vitest: add reporters verbose

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'node',
-    fileParallelism: false,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-      exclude: [
-        'node_modules/**',
-        'dist/**',
-        '**/*.test.ts',
-        '**/*.spec.ts',
-        '**/__tests__/**'
-      ]
-    }
-  }
-})
+    test: {
+        globals: true,
+        environment: "node",
+        fileParallelism: false,
+        reporters: ["verbose"],
+        coverage: {
+            provider: "v8",
+            reporter: ["text", "html"],
+            exclude: [
+                "node_modules/**",
+                "dist/**",
+                "**/*.test.ts",
+                "**/*.spec.ts",
+                "**/__tests__/**",
+            ],
+        },
+    },
+});
