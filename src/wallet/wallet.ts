13236b11d261d2d6916e7a622dbc31bbf3b3c111
louisinger <louis@vulpem.com>
2025-10-13 09:26:21 +0200

    fix

--- src/wallet/wallet.ts
+++ src/wallet/wallet.ts
@@ -1114,7 +1114,6 @@ export class Wallet implements IWallet {
 
             // boarding utxo, we need to sign the settlement tx
             if (!vtxo) {
-                const inputIndexes: number[] = [];
                 for (let i = 0; i < settlementPsbt.inputsLength; i++) {
                     const settlementInput = settlementPsbt.getInput(i);
 
@@ -1133,13 +1132,12 @@ export class Wallet implements IWallet {
                     settlementPsbt.updateInput(i, {
                         tapLeafScript: [input.forfeitTapLeafScript],
                     });
-                    inputIndexes.push(i);
+                    settlementPsbt = await this.identity.sign(settlementPsbt, [
+                        i,
+                    ]);
+                    hasBoardingUtxos = true;
+                    break;
                 }
-                settlementPsbt = await this.identity.sign(
-                    settlementPsbt,
-                    inputIndexes
-                );
-                hasBoardingUtxos = true;
 
                 continue;
             }
