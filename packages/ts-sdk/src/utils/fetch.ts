8224a35a8d0d438d650ef22354f90d587eaa55ba
João Bordalo <bordalix@users.noreply.github.com>
2026-06-11 16:56:40 +0100

    arkana fixes

--- packages/ts-sdk/src/utils/fetch.ts
+++ packages/ts-sdk/src/utils/fetch.ts
@@ -4,11 +4,7 @@ export function fetch(input: RequestInfo, init?: RequestInit): Promise<Response>
     if (typeof globalThis.fetch !== "function") {
         throw new Error("Fetch API is not available in this environment.");
     }
-    if (init) {
-        init.headers = {
-            ...init.headers,
-            "X-Build-Version": version,
-        };
-    }
-    return globalThis.fetch(input, init);
+    const headers = new Headers(init?.headers);
+    headers.set("X-Build-Version", version);
+    return globalThis.fetch(input, { ...init, headers });
 }
